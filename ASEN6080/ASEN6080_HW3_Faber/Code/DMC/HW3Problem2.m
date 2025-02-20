8937e5d407c6d3c1d394969a0d5f2f3a8c576954
ianfaber <iafa3639@colorado.edu>
2025-02-20 15:23:16 -0700

    Initial HW 3 commit

---
+++ ASEN6080/ASEN6080_HW3_Faber/Code/DMC/HW3Problem2.m
@@ -0,0 +1,48 @@
+%% ASEN 6080 HW 3 Problem 2 Main Script
+% By: Ian Faber
+
+%% Housekeeping
+clc; clear; close all;
+
+%% Setup
+addpath("..\")
+addpath(genpath("..\..\Utilities\"));
+
+    % Extract parameters for Earth
+earthConst = getPlanetConst();
+
+    % Set up stations
+stations = makeStations(earthConst);
+
+    % Extract orbital elements
+orbital = getOrbitConst();
+
+%% Make Truth Data
+x0Perturb = 1*0.5*[1 1 1 1e-3 1e-3 1e-3]';
+numOrbits = 15;
+dt = 10; % sec
+filename = "HW3Problem1Data.mat";
+
+    % Only generate data once
+if true
+    generateTruthData_MuJ2J3(earthConst, orbital, 0*x0Perturb, stations, filename, numOrbits, dt);
+end
+
+load("HW3Problem1Data.mat");
+
+titleText = sprintf("Measurement data");
+xLabel = "Time [sec]"; 
+yLabel = ["\rho [km]", "\rhoDot [km/s]", "Elevation Angle [deg]"];
+plotMeasurements(stations, titleText, xLabel, yLabel);
+
+%% Problem 1b: Filter setup
+sigR = 1; % km
+sigV = 1e-3; % km/s
+
+x0 = 1*0.5*[1 1 1 1e-3 1e-3 1e-3]';
+P0 = diag([sigR^2, sigR^2, sigR^2, sigV^2, sigV^2, sigV^2]);
+
+input("Press Enter to continue, 'Ctrl-C' to exit");
+
+
+
