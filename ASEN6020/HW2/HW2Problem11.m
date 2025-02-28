118f99c579dcb364895aee7a50ed35caa7271cfd
ianfaber <iafa3639@colorado.edu>
2025-02-28 14:47:01 -0700

    Setting up 6020 HW 2 Problem 11

---
+++ ASEN6020/HW2/HW2Problem11.m
@@ -0,0 +1,21 @@
+%% ASEN 6020 HW 2 Problem 11 Main Script
+% By: Ian Faber
+
+%% Housekeeping
+clc; clear; close all;
+
+%% Define constants and solution space
+    % Constants
+r1 = 10000; % km
+mu = 398600.4415; % km^3/s^2
+
+    % Solution space
+r = linspace(11.94,25,100);
+dTheta = linspace(0,pi,100); % rad
+
+%% Solve lambert's problem for every r, dTheta combo
+
+
+
+
+
