0bc539cb1ade3564784b1967b65d7b2e22939491
ianfaber <iafa3639@colorado.edu>
2025-09-21 12:23:34 -0600

    HW 3 Commit

--- ASEN5254/HW3/ASEN5254HW3Main.m
+++ ASEN5254/HW3/ASEN5254HW3Main.m
@@ -5,22 +5,32 @@
 clc; clear; close all;
 
 %% Setup
-testC3 = -1:0.01:1; % List of cos(theta3) to test
+testC1 = -1:1e-6:1; % List of cos(theta3) to test
 point = [0; 4]; % 
 
 %% Run procedure to solve for thetas and check answer
-thetas = solveThetas(point, testC3);
+thetas = solveThetas(point, testC1);
 
+figure; 
 for k = 1:length(thetas.theta1)
+    clf; hold on; grid on;
+    title("End Effector Position")
+
     theta1 = thetas.theta1(k);
     theta2 = thetas.theta2(k);
     theta3 = thetas.theta3(k);
 
     x = cos(theta1)*(cos(theta2)*(9*cos(theta3) + 8) - 9*sin(theta2)*sin(theta3) + 8) ...
       + sin(theta1)*(sin(theta2)*(-9*cos(theta3) - 8) - 9*cos(theta2)*sin(theta3));
-
+ 
     y = cos(theta1)*(sin(theta2)*(9*cos(theta3) + 8) + 9*cos(theta2)*sin(theta3)) ...
       + sin(theta1)*(cos(theta2)*(9*cos(theta3) + 8) - 9*sin(theta2)*sin(theta3) + 8);
+
+    plot(point(1), point(2), 'kx');
+    plot(x,y,'b.', 'MarkerSize', 15);
+    xlabel("x [m]"); ylabel("y [m]");
+    xlim([-20, 20]); ylim([-20, 20])
+    drawnow;
 
     fprintf("End effector with theta = [%.3f, %.3f, %.3f]: (x,y) = [%.3f, %.3f]\n", theta1, theta2, theta3, x, y)
 end
--- ASEN5254/HW3/solveThetas.m
+++ ASEN5254/HW3/solveThetas.m
@@ -1,9 +1,9 @@
-function thetas = solveThetas(point, testC3)
+function thetas = solveThetas(point, testC1)
 % Function that runs through the inverse kinematics procedure to solve for
 % thetas for the 3 link manipulator in HW 3
 %   Inputs:
 %       - point: Coordinates of the end effector in the inertial frame
-%       - testC3: Vector of cos(theta3) to calculate thetas for
+%       - testC1: Vector of cos(theta1) to calculate thetas for
 %   Outputs:
 %       - thetas: Calculated thetas for the provided cos(theta3) vector
 %
@@ -14,148 +14,74 @@ function thetas = solveThetas(point, testC3)
 x = point(1);
 y = point(2);
 
-% Function handles for fsolve
-s2Func = @(c2) sqrt(1-c2^2);
-c2Func = @(c2,c3,s3) (x^2 + y^2 - 144*c3 - 209 + 144*s2Func(c2)*s3)/(144*c3 + 128) - c2;
-
-s1Func = @(c1) sqrt(1-c1^2);
-c1Func = @(c1,s2,c2,s3,c3) (x - s1Func(c1)*(s2*(-9*c3 - 8) -9*c2*s3))/(c2*(9*c3 + 8) - 9*s2*s3 + 8) - c1;
-
-% Loop through all cos(theta3)'s
+% Assign empty outputs
 thetas.theta1 = [];
 thetas.theta2 = [];
 thetas.theta3 = [];
-for k = 1:length(testC3)
-        % Choose cos(theta3)
-    c3 = testC3(k);
 
-        % Positive and negative sin(theta3)
+% Loop over cos(theta1) values
+for k = 1:length(testC1)
+        % Extract cos(theta1)
+    c1 = testC1(k);
+
+        % Calculate sin(theta1)
     for kk = 1:2
+            % Positive or negative sin(theta1)
         if kk == 1
-            s3 = sqrt(1-(c3^2));
-                % Rootsolve for cos(theta2)
-            c2 = fsolve(@(c2)c2Func(c2, c3, s3), 1);
-
-            if abs(c2) > 1
-                continue;
-            end
-
-                % Positive and negative sin(theta2)
-            for idx = 1:2
-                if idx == 1
-                    s2 = sqrt(1-(c2^2));
-                        % Rootsolve for cos(theta1)
-                    c1 = fsolve(@(c1)c1Func(c1, s2, c2, s3, c3), 0);
-
-                    if abs(c1) > 1
-                        continue;
-                    end
-
-                        % Positive and negative sin(theta1)
-                    for idx2 = 1:2
-                        if idx2 == 1
-                            s1 = sqrt(1-(c1^2));
-                        else
-                            s1 = -sqrt(1-(c1^2));
-                        end
-                    end
-                else
-                    s2 = -sqrt(1-(c2^2));
-                        % Rootsolve for cos(theta1)
-                    c1 = fsolve(@(c1)c1Func(c1, s2, c2, s3, c3), 0);
-
-                    if abs(c1) > 1
-                        continue;
-                    end
-
-                        % Positive and negative sin(theta1)
-                    for idx2 = 1:2
-                        if idx2 == 1
-                            s1 = sqrt(1-(c1^2));
-                        else
-                            s1 = -sqrt(1-(c1^2));
-                        end
-                    end
-                end
-            end
+            s1 = sqrt(1-c1^2);
         else
-            s3 = -sqrt(1-(c3^2));
+            s1 = -sqrt(1-c1^2);
+        end
 
-                % Rootsolve for cos(theta2)
-            c2 = fsolve(@(c2)c2Func(c2, c3, s3), 0);
+            % Calculate xPrime and yPrime
+        xPrime = x*c1 + y*s1 + 8*c1;
+        yPrime = -x*s1 + y*c1 - 8*s1;
 
-            if abs(c2) > 1
-                continue;
+            % Calculate cos(theta3)
+        c3 = (xPrime^2 + yPrime^2 - 145)/144;
+
+            % Calculate sin(theta3)
+        for idx = 1:2
+                % Positive or negative sin(theta3)
+            if idx == 1
+                s3 = sqrt(1-c3^2);
+            else
+                s3 = -sqrt(1-c3^2);
             end
 
-                % Positive and negative sin(theta2)
-            for idx = 1:2
-                if idx == 1
-                    s2 = sqrt(1-(c2^2));
+                % Calculate cos(theta2)
+            c2 = (xPrime*(9*c3 + 8) + 9*yPrime*s3)/(xPrime^2 + yPrime^2);
 
-                        % Rootsolve for cos(theta1)
-                    if (c2*(9*c3 + 8) - 9*s2*s3 + 8) == 0
-                        continue;
-                    end
-
-                    c1 = fsolve(@(c1)c1Func(c1, s2, c2, s3, c3), 0);
-
-                    if abs(c1) > 1
-                        continue;
-                    end
-
-                        % Positive and negative sin(theta1)
-                    for idx2 = 1:2
-                        if idx2 == 1
-                            s1 = sqrt(1-(c1^2));
-                        else
-                            s1 = -sqrt(1-(c1^2));
-                        end
-                    end
+                % Calculate sin(theta2)
+            for idx2 = 1:2
+                if idx2 == 1
+                    s2 = sqrt(1-c2^2);
                 else
-                    s2 = -sqrt(1-(c2^2));
-                        % Rootsolve for cos(theta1)
-                    if (c2*(9*c3 + 8) - 9*s2*s3 + 8) == 0
-                        continue;
-                    end
+                    s2 = -sqrt(1-c2^2);
+                end
 
-                    c1 = fsolve(@(c1)c1Func(c1, s2, c2, s3, c3), 0);
-                    
-                    if abs(c1) > 1
-                        continue;
-                    end
+                    % Assign outputs
+                theta1 = atan2(s1, c1);
+                theta2 = atan2(s2, c2);
+                theta3 = atan2(s3, c3);
 
-                        % Positive and negative sin(theta1)
-                    for idx2 = 1:2
-                        if idx2 == 1
-                            s1 = sqrt(1-(c1^2));
-                        else
-                            s1 = -sqrt(1-(c1^2));
-                        end
-                    end
+                %     % Check result
+                xCheck = cos(theta1)*(cos(theta2)*(9*cos(theta3) + 8) - 9*sin(theta2)*sin(theta3) + 8) ...
+                       + sin(theta1)*(sin(theta2)*(-9*cos(theta3) - 8) - 9*cos(theta2)*sin(theta3));
+
+                yCheck = cos(theta1)*(sin(theta2)*(9*cos(theta3) + 8) + 9*cos(theta2)*sin(theta3)) ...
+                       + sin(theta1)*(cos(theta2)*(9*cos(theta3) + 8) - 9*sin(theta2)*sin(theta3) + 8);
+
+                epsilon = 1e-10n;
+                if (xCheck <= point(1) + epsilon && xCheck >= point(1) - epsilon && yCheck <= point(2) + epsilon && yCheck >= point(2) - epsilon)
+                    thetas.theta1 = [thetas.theta1; theta1];
+                    thetas.theta2 = [thetas.theta2; theta2];
+                    thetas.theta3 = [thetas.theta3; theta3];
                 end
             end
         end
     end
-
-    try
-            % Invalidate answer if imaginary angles
-        if any(abs(imag([s1, c1, s2, c2, s3, c3])) > 0)
-            continue;
-        end
-    
-            % Calculate outputs
-        theta1 = atan2(s1, c1);
-        theta2 = atan2(s2, c2);
-        theta3 = atan2(s3, c3);
-    
-            % Assign outputs
-        thetas.theta1 = [thetas.theta1; theta1];
-        thetas.theta2 = [thetas.theta2; theta2];
-        thetas.theta3 = [thetas.theta3; theta3];
-    catch
-        continue;
-    end
+end
 
 end
 
